4281ed6882684fa38389e5006a751409f430fb92
seongEun9 <p99.eun@gmail.com>
2024-08-09 16:39:27 +0900

    24년 8월 9일 오전 커밋 (고객센터 자주묻는 질문 등록/수정/삭제 완료 )

--- src/main/java/org/zerock/chain/controller/UserController.java
+++ src/main/java/org/zerock/chain/controller/UserController.java
@@ -23,16 +23,19 @@ public class UserController {
         this.favoriteQnaService = favoriteQnaService;
     }
 
+    // 마이페이지로 이동
     @GetMapping("/mypage")
     public String userMypage(Model model) {
         return "user/mypage";
     }
 
+    // 설정 페이지로 이동
     @GetMapping("/setting")
     public String userSetting(Model model) {
         return "user/setting";
     }
 
+    // 모든 FAQ 목록을 가져와서 Q&A 페이지로 이동
     @GetMapping("/Q&A")
     public String getAllFAQs(Model model) {
         List<FavoriteQnaDTO> faqs = favoriteQnaService.getAllFAQs();
@@ -40,6 +43,7 @@ public class UserController {
         return "user/Q&A";
     }
 
+   // 새로운 FAQ 등록
     @PostMapping("/add")
     public String registerFAQ(@RequestParam String faqName, @RequestParam String faqContent, Model model) {
         FavoriteQnaRequestDTO requestDTO = new FavoriteQnaRequestDTO();
@@ -50,38 +54,45 @@ public class UserController {
         return "redirect:/user/Q&A";
     }
 
-    @PostMapping("/edit")
+    // 기존 FAQ 수정
+    @PostMapping("/faq/edit")
     public String editFaq(@RequestParam("faqNo") Long faqNo, @RequestParam("faqName") String faqName, @RequestParam("faqContent") String faqContent) {
         favoriteQnaService.updateFaq(faqNo, faqName, faqContent);
         return "redirect:/user/Q&A";
     }
 
-    @PostMapping("/delete")
+    //FAQ 삭제
+    @PostMapping("/faq/delete")
     public String deleteFaq(@RequestParam("faqNo") Long faqNo) {
         favoriteQnaService.deleteFaq(faqNo);
         return "redirect:/user/Q&A";
     }
 
+   // 질문 등록 페이지로 이동
     @GetMapping("/qaRegister")
     public String userQARegister(Model model) {
         return "user/qaRegister";
     }
 
+    // 질문 상세 페이지로 이동
     @GetMapping("/qaDetail")
     public String userQADetail(Model model) {
         return "user/qaDetail";
     }
 
+    // 로그아웃 페이지로 이동
     @GetMapping("/logout")
     public String userLogout(Model model) {
         return "user/logout";
     }
 
+    //로그인 페이지로 이동
     @GetMapping("/login")
     public String userLogin(Model model) {
         return "user/login";
     }
 
+    // 알림 페이지로 이동
     @GetMapping("/alarm")
     public String userAlarm(Model model) {
         return "user/alarm";
