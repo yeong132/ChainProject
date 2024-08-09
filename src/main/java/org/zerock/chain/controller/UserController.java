c79313ccfc696e2212cd29e3bc0d058dc61843a2
seongEun9 <p99.eun@gmail.com>
2024-08-09 16:10:32 +0900

    24년 8월 9일 오전 커밋 (고객센터 등록완료, 수정삭제 기능 구현중)

--- src/main/java/org/zerock/chain/controller/UserController.java
+++ src/main/java/org/zerock/chain/controller/UserController.java
@@ -1,65 +1,89 @@
 package org.zerock.chain.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.zerock.chain.dto.FavoriteQnaDTO;
+import org.zerock.chain.dto.FavoriteQnaRequestDTO;
+import org.zerock.chain.service.FavoriteQnaService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
 @Log4j2
 public class UserController {
 
-    //  마이페이지 페이지
+    private final FavoriteQnaService favoriteQnaService;
+
+    @Autowired
+    public UserController(FavoriteQnaService favoriteQnaService) {
+        this.favoriteQnaService = favoriteQnaService;
+    }
+
     @GetMapping("/mypage")
     public String userMypage(Model model) {
         return "user/mypage";
     }
 
-    //  환경설정 페이지
     @GetMapping("/setting")
-    public String UserSetting(Model model) {
+    public String userSetting(Model model) {
         return "user/setting";
     }
 
-
-
-    //  고객센터 페이지 조회 (전체목록조회)
     @GetMapping("/Q&A")
-    public String UserQAList(Model model) {
+    public String getAllFAQs(Model model) {
+        List<FavoriteQnaDTO> faqs = favoriteQnaService.getAllFAQs();
+        model.addAttribute("faqs", faqs);
         return "user/Q&A";
     }
 
-    //  고객센터  작성 페이지 (등록)
+    @PostMapping("/add")
+    public String registerFAQ(@RequestParam String faqName, @RequestParam String faqContent, Model model) {
+        FavoriteQnaRequestDTO requestDTO = new FavoriteQnaRequestDTO();
+        requestDTO.setFaqName(faqName);
+        requestDTO.setFaqContent(faqContent);
+
+        favoriteQnaService.createFAQ(requestDTO);
+        return "redirect:/user/Q&A";
+    }
+
+    @PostMapping("/edit")
+    public String editFaq(@RequestParam("faqNo") Long faqNo, @RequestParam("faqName") String faqName, @RequestParam("faqContent") String faqContent) {
+        favoriteQnaService.updateFaq(faqNo, faqName, faqContent);
+        return "redirect:/user/Q&A";
+    }
+
+    @PostMapping("/delete")
+    public String deleteFaq(@RequestParam("faqNo") Long faqNo) {
+        favoriteQnaService.deleteFaq(faqNo);
+        return "redirect:/user/Q&A";
+    }
+
     @GetMapping("/qaRegister")
-    public String UserQARegister(Model model) {
+    public String userQARegister(Model model) {
         return "user/qaRegister";
     }
 
-    //  고객센터 상세 조회 페이지
-    //  고객센터 상세 조회 페이지 (수정/삭제)
     @GetMapping("/qaDetail")
-    public String UserQADetail(Model model) {
+    public String userQADetail(Model model) {
         return "user/qaDetail";
     }
 
-
-
-
-    //  로그아웃 페이지
     @GetMapping("/logout")
-    public String UserLogout(Model model) {return "user/logout";}
+    public String userLogout(Model model) {
+        return "user/logout";
+    }
 
-    //  로그인 페이지
     @GetMapping("/login")
-    public String UserLogin(Model model) {
+    public String userLogin(Model model) {
         return "user/login";
     }
 
-    //  환경설정 페이지
     @GetMapping("/alarm")
-    public String UserAlarm(Model model) {
+    public String userAlarm(Model model) {
         return "user/alarm";
     }
 }
