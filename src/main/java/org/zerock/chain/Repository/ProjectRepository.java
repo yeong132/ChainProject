638b125b571f23fb84651be3034df0dbc48f91d1
edu0010\edu0011 <choh_56@naver.com>
2024-08-12 18:47:31 +0900

    짭브랜치

--- src/main/java/org/zerock/chain/Repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package org.zerock.chain.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.zerock.chain.model.Project;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
