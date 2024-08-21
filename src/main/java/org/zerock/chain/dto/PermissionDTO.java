2b4419b49d4d73a2b4d38e6e024202403bfedb6c
MohuttTT <zolloz2007@gmail.com>
2024-08-21 17:50:21 +0900

    임시커밋

---
+++ src/main/java/org/zerock/chain/dto/PermissionDTO.java
@@ -0,0 +1,17 @@
+package org.zerock.chain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PermissionDTO {
+
+
+    private Long perNo;
+    private String perName;
+}
