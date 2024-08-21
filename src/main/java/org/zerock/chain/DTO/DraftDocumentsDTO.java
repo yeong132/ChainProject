e90d50c4c2520f818f0a91432d4c1dcce2701dbf
MohuttTT <zolloz2007@gmail.com>
2024-08-21 16:25:27 +0900

    generated 폴더 삭제, DTO -> dto, Service -> serivce 수정

--- src/main/java/org/zerock/chain/DTO/DraftDocumentsDTO.java
+++
@@ -1,5 +0,0 @@
-package org.zerock.chain.dto;
-
-public class DraftDocumentsDTO extends DocumentsDTO {
-    // 문서 상태는 삭제하도록 처리
-}
--- src/main/java/org/zerock/chain/DTO/SentDocumentsDTO.java
+++
@@ -1,4 +0,0 @@
-package org.zerock.chain.dto;
-
-public class SentDocumentsDTO extends DocumentsDTO {
-}
