1eb5d65a441fa81b445689b0aef01d9464c79f3a
seongEun9 <p99.eun@gmail.com>
2024-08-23 16:10:20 +0900

    24/08/23 사원번호 조회 가능 <p>사원번호 : <span th:text="${session.empNo}"></span></p> 방식 사용하면 가능

--- src/main/java/org/zerock/chain/junhyuck/repository/SignupRepository.java
+++ src/main/java/org/zerock/chain/junhyuck/repository/SignupRepository.java
@@ -1,7 +1,7 @@
 package org.zerock.chain.junhyuck.repository;
 
-import org.zerock.chain.junhyuck.model.Signup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.zerock.chain.junhyuck.model.Signup;
 
 import java.util.Optional;
 
