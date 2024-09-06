ef8a6034e841fb6dc66ce4b180b813e25890af93
박영민 <pfyrmog6964@gmail.com>
2024-09-06 16:15:39 +0900

    file->projectFiles으로 수정, 프로젝트에 첨부파일 기능 추가!

--- src/main/java/org/zerock/chain/pse/service/ProjectService.java
+++ src/main/java/org/zerock/chain/pse/service/ProjectService.java
@@ -5,6 +5,7 @@ import org.zerock.chain.pse.dto.ProjectDTO;
 import org.zerock.chain.pse.dto.ProjectRequestDTO;
 
 
+import java.io.IOException;
 import java.time.YearMonth;
 import java.util.List;
 import java.util.Map;
@@ -16,10 +17,12 @@ public interface ProjectService {
     void setProjectFavorite(Long projectNo, boolean projectFavorite); // 즐겨찾기 갱신
     void updateProjectProgress(Long projectNo, Integer projectProgress);  // 진행도 업데이트
     ProjectDTO getProjectById(Long projectNo);  // 특정 프로젝트 조회
- void updateProject(Long projectNo, ProjectRequestDTO projectRequestDTO);  // 프로젝트 수정
+    void updateProject(Long projectNo, ProjectRequestDTO projectRequestDTO);  // 프로젝트 수정
     void deleteProject(Long projectNo); // 삭제 기능
     String saveFile(MultipartFile file) throws Exception; // 첨부파일 저장
     List<ProjectDTO> getTemporaryProjects(); // 임시 보관 프로젝트 조회
     // 특정 사용자가 참여 중인 프로젝트 목록 조회
     List<ProjectDTO> getProjectsByUser(Long empNo);
+
+    String uploadFile(MultipartFile file) throws IOException; // 파일 업로드 및 저장 처리 메서드
 }
